65282a49d593822bed96e5f99579a5fdaac696f6
JavierPera00 <pera.vidal.javier@gmail.com>
2026-02-06 11:17:30 +0100

    x

--- Angular/Frontend/src/app/app.routes.ts
+++ Angular/Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Mapa } from './mapa/mapa';
+import { Noticias } from './noticias/noticias';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Mapa },
+  { path: 'mapa', component: Mapa },
+  { path: 'noticias', component: Noticias },
+  { path: '**', redirectTo: '/home' }
+];
--- Angular/Frontend/src/app/app.ts
+++ Angular/Frontend/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
